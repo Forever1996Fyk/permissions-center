b37801c52488bc631440b9b7c123f15ac2286c5a
YuKaiFan <michaelkai@aliyun.com>
2021-12-03 17:15:01 +0800

    feat: 框架搭建

---
+++ perm-biz/src/main/java/com/ah/cloud/perm/biz/infrastructure/application/service/PermRoleService.java
@@ -0,0 +1,16 @@
+package com.ah.cloud.perm.biz.infrastructure.application.service;
+
+import com.ah.cloud.perm.biz.infrastructure.repository.bean.PermRole;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 权限角色表 服务类
+ * </p>
+ *
+ * @author auto_generation
+ * @since 2021-12-03
+ */
+public interface PermRoleService extends IService<PermRole> {
+
+}
---
+++ perm-biz/src/main/java/com/ah/cloud/perm/biz/infrastructure/application/service/impl/PermRoleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ah.cloud.perm.biz.infrastructure.application.service.impl;
+
+import com.ah.cloud.perm.biz.infrastructure.repository.bean.PermRole;
+import com.ah.cloud.perm.biz.infrastructure.repository.dao.PermRoleMapper;
+import com.ah.cloud.perm.biz.infrastructure.application.service.PermRoleService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 权限角色表 服务实现类
+ * </p>
+ *
+ * @author auto_generation
+ * @since 2021-12-03
+ */
+@Service
+public class PermRoleServiceImpl extends ServiceImpl<PermRoleMapper, PermRole> implements PermRoleService {
+
+}

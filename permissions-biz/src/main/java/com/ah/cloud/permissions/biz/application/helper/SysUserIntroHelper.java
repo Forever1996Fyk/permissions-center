0e2b0befd84812154c1734d47051a7c0cf5e3633
yukaifan <michaelkai@aliyun.com>
2022-09-11 11:02:18 +0800

    代码优化

--- permissions-biz/src/main/java/com/ah/cloud/permissions/biz/application/helper/SysUserIntroHelper.java
+++ permissions-biz/src/main/java/com/ah/cloud/permissions/biz/application/helper/SysUserIntroHelper.java
@@ -1,85 +1,85 @@
-//package com.ah.cloud.permissions.biz.application.helper;
-//
-//import com.ah.cloud.permissions.biz.domain.user.intro.form.SysUserIntroAddForm;
-//import com.ah.cloud.permissions.biz.domain.user.intro.form.SysUserIntroUpdateForm;
-//import com.ah.cloud.permissions.biz.domain.user.intro.vo.SysUserIntroVo;
-//import com.ah.cloud.permissions.biz.infrastructure.repository.bean.SysDept;
-//import com.ah.cloud.permissions.biz.infrastructure.repository.bean.SysUser;
-//import com.ah.cloud.permissions.biz.infrastructure.repository.bean.SysUserIntro;
-//import com.ah.cloud.permissions.biz.infrastructure.util.SecurityUtils;
-//import org.mapstruct.Mapper;
-//import org.mapstruct.Mapping;
-//import org.mapstruct.Mappings;
-//import org.mapstruct.factory.Mappers;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * @program: permissions-center
-// * @description:
-// * @author: YuKai Fan
-// * @create: 2022-08-19 19:07
-// **/
-//@Component
-//public class SysUserIntroHelper {
-//
-//    /**
-//     * 数据转换
-//     * @param form
-//     * @return
-//     */
-//    public SysUserIntro convert(SysUserIntroAddForm form) {
-//        SysUserIntro sysUserIntro = Convert.INSTANCE.convert(form);
-//        sysUserIntro.setUserId(SecurityUtils.getUserIdBySession());
-//        sysUserIntro.setCreator(SecurityUtils.getUserNameBySession());
-//        sysUserIntro.setModifier(SecurityUtils.getUserNameBySession());
-//        return sysUserIntro;
-//    }
-//
-//    /**
-//     * 数据转换
-//     * @param form
-//     * @return
-//     */
-//    public SysUserIntro convert(SysUserIntroUpdateForm form) {
-//        return Convert.INSTANCE.convert(form);
-//    }
-//
-//    /**
-//     * 数据转换
-//     * @param sysUserIntro
-//     * @param sysDept
-//     * @param sysUser
-//     * @return
-//     */
-//    public SysUserIntroVo convertToVo(SysUserIntro sysUserIntro) {
-//        return Convert.INSTANCE.convertToVo(sysUserIntro);
-//    }
-//
-//    @Mapper
-//    public interface Convert {
-//        SysUserIntroHelper.Convert INSTANCE = Mappers.getMapper(SysUserIntroHelper.Convert.class);
-//
-//        /**
-//         * 数据转换
-//         * @param form
-//         * @return
-//         */
-//        SysUserIntro convert(SysUserIntroAddForm form);
-//
-//        /**
-//         * 数据转换
-//         * @param form
-//         * @return
-//         */
-//        @Mappings({})
-//        @Mapping(target = "birthDay", expression = "java(com.ah.cloud.permissions.biz.infrastructure.util.DateUtils.parse(form.getBirthDay()))")
-//        SysUserIntro convert(SysUserIntroUpdateForm form);
-//
-//        /**
-//         * 数据转换
-//         * @param sysUserIntro
-//         * @return
-//         */
-//        SysUserIntroVo convertToVo(SysUserIntro sysUserIntro);
-//    }
-//}
+package com.ah.cloud.permissions.biz.application.helper;
+
+import com.ah.cloud.permissions.biz.domain.user.intro.form.SysUserIntroAddForm;
+import com.ah.cloud.permissions.biz.domain.user.intro.form.SysUserIntroUpdateForm;
+import com.ah.cloud.permissions.biz.domain.user.intro.vo.SysUserIntroVo;
+import com.ah.cloud.permissions.biz.infrastructure.repository.bean.SysDept;
+import com.ah.cloud.permissions.biz.infrastructure.repository.bean.SysUser;
+import com.ah.cloud.permissions.biz.infrastructure.repository.bean.SysUserIntro;
+import com.ah.cloud.permissions.biz.infrastructure.util.SecurityUtils;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: permissions-center
+ * @description:
+ * @author: YuKai Fan
+ * @create: 2022-08-19 19:07
+ **/
+@Component
+public class SysUserIntroHelper {
+
+    /**
+     * 数据转换
+     * @param form
+     * @return
+     */
+    public SysUserIntro convert(SysUserIntroAddForm form) {
+        SysUserIntro sysUserIntro = Convert.INSTANCE.convert(form);
+        sysUserIntro.setUserId(SecurityUtils.getUserIdBySession());
+        sysUserIntro.setCreator(SecurityUtils.getUserNameBySession());
+        sysUserIntro.setModifier(SecurityUtils.getUserNameBySession());
+        return sysUserIntro;
+    }
+
+    /**
+     * 数据转换
+     * @param form
+     * @return
+     */
+    public SysUserIntro convert(SysUserIntroUpdateForm form) {
+        return Convert.INSTANCE.convert(form);
+    }
+
+    /**
+     * 数据转换
+     * @param sysUserIntro
+     * @param sysDept
+     * @param sysUser
+     * @return
+     */
+    public SysUserIntroVo convertToVo(SysUserIntro sysUserIntro) {
+        return Convert.INSTANCE.convertToVo(sysUserIntro);
+    }
+
+    @Mapper
+    public interface Convert {
+        SysUserIntroHelper.Convert INSTANCE = Mappers.getMapper(SysUserIntroHelper.Convert.class);
+
+        /**
+         * 数据转换
+         * @param form
+         * @return
+         */
+        SysUserIntro convert(SysUserIntroAddForm form);
+
+        /**
+         * 数据转换
+         * @param form
+         * @return
+         */
+        @Mappings({})
+        @Mapping(target = "birthDay", expression = "java(com.ah.cloud.permissions.biz.infrastructure.util.DateUtils.parse(form.getBirthDay()))")
+        SysUserIntro convert(SysUserIntroUpdateForm form);
+
+        /**
+         * 数据转换
+         * @param sysUserIntro
+         * @return
+         */
+        SysUserIntroVo convertToVo(SysUserIntro sysUserIntro);
+    }
+}
--- permissions-biz/src/main/java/com/ah/cloud/permissions/biz/application/manager/SysUserIntroManager.java
+++ permissions-biz/src/main/java/com/ah/cloud/permissions/biz/application/manager/SysUserIntroManager.java
@@ -1,73 +1,73 @@
-//package com.ah.cloud.permissions.biz.application.manager;
-//
-//import com.ah.cloud.permissions.biz.application.helper.SysUserIntroHelper;
-//import com.ah.cloud.permissions.biz.application.service.ext.SysUserIntroExtService;
-//import com.ah.cloud.permissions.biz.domain.user.intro.form.SysUserIntroUpdateForm;
-//import com.ah.cloud.permissions.biz.domain.user.intro.vo.SysUserIntroVo;
-//import com.ah.cloud.permissions.biz.infrastructure.constant.PermissionsConstants;
-//import com.ah.cloud.permissions.biz.infrastructure.exception.BizException;
-//import com.ah.cloud.permissions.biz.infrastructure.repository.bean.SysUserIntro;
-//import com.ah.cloud.permissions.biz.infrastructure.util.SecurityUtils;
-//import com.ah.cloud.permissions.enums.common.ErrorCodeEnum;
-//import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Component;
-//
-//import javax.annotation.Resource;
-//import java.util.Objects;
-//
-///**
-// * @program: permissions-center
-// * @description:
-// * @author: YuKai Fan
-// * @create: 2022-08-19 18:57
-// **/
-//@Slf4j
-//@Component
-//public class SysUserIntroManager {
-//    @Resource
-//    private SysUserIntroHelper sysUserIntroHelper;
-//    @Resource
-//    private SysUserIntroExtService sysUserIntroExtService;
-//
-//    /**
-//     * 更新用户信息
-//     * @param form
-//     */
-//    public void updateIntro(SysUserIntroUpdateForm form) {
-//        Long userId = SecurityUtils.getUserIdBySession();
-//        SysUserIntro existSysUserIntro = sysUserIntroExtService.getOneByUserId(userId);
-//        SysUserIntro sysUserIntro = sysUserIntroHelper.convert(form);
-//        sysUserIntro.setUserId(userId);
-//        // 这里无需担心重新请求问题, 因为userId是唯一索引，所以不会出现问题
-//        if (Objects.isNull(existSysUserIntro)) {
-//            String userNameBySession = SecurityUtils.getUserNameBySession();
-//            sysUserIntro.setCreator(userNameBySession);
-//            sysUserIntro.setModifier(userNameBySession);
-//            sysUserIntroExtService.save(sysUserIntro);
-//        } else {
-//            sysUserIntro.setVersion(existSysUserIntro.getVersion());
-//            boolean updateResult = sysUserIntroExtService.update(
-//                    sysUserIntro,
-//                    new UpdateWrapper<SysUserIntro>().lambda()
-//                            .eq(SysUserIntro::getId, existSysUserIntro.getId())
-//                            .eq(SysUserIntro::getVersion, existSysUserIntro.getVersion())
-//            );
-//            if (!updateResult) {
-//                throw new BizException(ErrorCodeEnum.VERSION_ERROR);
-//            }
-//        }
-//    }
-//
-//    /**
-//     * 根据用户id查询用户简介
-//     * @return
-//     */
-//    public SysUserIntroVo findSysUserIntroVoByUserId(Long userId) {
-//        if (Objects.equals(userId, PermissionsConstants.ZERO) || userId == null) {
-//            userId = SecurityUtils.getUserIdBySession();
-//        }
-//        SysUserIntro sysUserIntro = sysUserIntroExtService.getOneByUserId(userId);
-//        return sysUserIntroHelper.convertToVo(sysUserIntro);
-//    }
-//}
+package com.ah.cloud.permissions.biz.application.manager;
+
+import com.ah.cloud.permissions.biz.application.helper.SysUserIntroHelper;
+import com.ah.cloud.permissions.biz.application.service.ext.SysUserIntroExtService;
+import com.ah.cloud.permissions.biz.domain.user.intro.form.SysUserIntroUpdateForm;
+import com.ah.cloud.permissions.biz.domain.user.intro.vo.SysUserIntroVo;
+import com.ah.cloud.permissions.biz.infrastructure.constant.PermissionsConstants;
+import com.ah.cloud.permissions.biz.infrastructure.exception.BizException;
+import com.ah.cloud.permissions.biz.infrastructure.repository.bean.SysUserIntro;
+import com.ah.cloud.permissions.biz.infrastructure.util.SecurityUtils;
+import com.ah.cloud.permissions.enums.common.ErrorCodeEnum;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Objects;
+
+/**
+ * @program: permissions-center
+ * @description:
+ * @author: YuKai Fan
+ * @create: 2022-08-19 18:57
+ **/
+@Slf4j
+@Component
+public class SysUserIntroManager {
+    @Resource
+    private SysUserIntroHelper sysUserIntroHelper;
+    @Resource
+    private SysUserIntroExtService sysUserIntroExtService;
+
+    /**
+     * 更新用户信息
+     * @param form
+     */
+    public void updateIntro(SysUserIntroUpdateForm form) {
+        Long userId = SecurityUtils.getUserIdBySession();
+        SysUserIntro existSysUserIntro = sysUserIntroExtService.getOneByUserId(userId);
+        SysUserIntro sysUserIntro = sysUserIntroHelper.convert(form);
+        sysUserIntro.setUserId(userId);
+        // 这里无需担心重新请求问题, 因为userId是唯一索引，所以不会出现问题
+        if (Objects.isNull(existSysUserIntro)) {
+            String userNameBySession = SecurityUtils.getUserNameBySession();
+            sysUserIntro.setCreator(userNameBySession);
+            sysUserIntro.setModifier(userNameBySession);
+            sysUserIntroExtService.save(sysUserIntro);
+        } else {
+            sysUserIntro.setVersion(existSysUserIntro.getVersion());
+            boolean updateResult = sysUserIntroExtService.update(
+                    sysUserIntro,
+                    new UpdateWrapper<SysUserIntro>().lambda()
+                            .eq(SysUserIntro::getId, existSysUserIntro.getId())
+                            .eq(SysUserIntro::getVersion, existSysUserIntro.getVersion())
+            );
+            if (!updateResult) {
+                throw new BizException(ErrorCodeEnum.VERSION_ERROR);
+            }
+        }
+    }
+
+    /**
+     * 根据用户id查询用户简介
+     * @return
+     */
+    public SysUserIntroVo findSysUserIntroVoByUserId(Long userId) {
+        if (Objects.equals(userId, PermissionsConstants.ZERO) || userId == null) {
+            userId = SecurityUtils.getUserIdBySession();
+        }
+        SysUserIntro sysUserIntro = sysUserIntroExtService.getOneByUserId(userId);
+        return sysUserIntroHelper.convertToVo(sysUserIntro);
+    }
+}

01a9e73ab4b700cd008cf31470d6f903fa9f433b
yukaifan <michaelkai@aliyun.com>
2022-10-17 09:37:21 +0800

    feat: 新增minio文件上传方式

---
+++ permissions-biz/src/main/java/com/ah/cloud/permissions/biz/application/service/CfgEncryptService.java
@@ -0,0 +1,16 @@
+package com.ah.cloud.permissions.biz.application.service;
+
+import com.ah.cloud.permissions.biz.infrastructure.repository.bean.CfgEncrypt;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 加密数据配置表 服务类
+ * </p>
+ *
+ * @author auto_generation
+ * @since 2022-10-11
+ */
+public interface CfgEncryptService extends IService<CfgEncrypt> {
+
+}
---
+++ permissions-biz/src/main/java/com/ah/cloud/permissions/biz/infrastructure/repository/mapper/CfgEncryptMapper.java
@@ -0,0 +1,16 @@
+package com.ah.cloud.permissions.biz.infrastructure.repository.mapper;
+
+import com.ah.cloud.permissions.biz.infrastructure.repository.bean.CfgEncrypt;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 加密数据配置表 Mapper 接口
+ * </p>
+ *
+ * @author auto_generation
+ * @since 2022-10-11
+ */
+public interface CfgEncryptMapper extends BaseMapper<CfgEncrypt> {
+
+}

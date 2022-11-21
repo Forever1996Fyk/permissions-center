c78e662a2ae7a6bc56c78ad66ca413ab7c3d84bc
yukaifan <michaelkai@aliyun.com>
2022-11-21 09:36:25 +0800

    代码合并

---
+++ permissions-biz/src/main/java/com/ah/cloud/permissions/biz/application/service/impl/ImSessionServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ah.cloud.permissions.biz.application.service.impl;
+
+import com.ah.cloud.permissions.biz.infrastructure.repository.bean.ImSession;
+import com.ah.cloud.permissions.biz.infrastructure.repository.mapper.ImSessionMapper;
+import com.ah.cloud.permissions.biz.application.service.ImSessionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * IM用户session表 服务实现类
+ * </p>
+ *
+ * @author auto_generation
+ * @since 2022-10-28
+ */
+@Service
+public class ImSessionServiceImpl extends ServiceImpl<ImSessionMapper, ImSession> implements ImSessionService {
+
+}
---
+++ permissions-biz/src/main/java/com/ah/cloud/permissions/biz/infrastructure/repository/mapper/ImSessionMapper.java
@@ -0,0 +1,16 @@
+package com.ah.cloud.permissions.biz.infrastructure.repository.mapper;
+
+import com.ah.cloud.permissions.biz.infrastructure.repository.bean.ImSession;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * IM用户session表 Mapper 接口
+ * </p>
+ *
+ * @author auto_generation
+ * @since 2022-10-28
+ */
+public interface ImSessionMapper extends BaseMapper<ImSession> {
+
+}
